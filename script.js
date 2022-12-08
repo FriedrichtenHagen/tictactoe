8238c9af8f55279f576301642262246417e4d3e1
Friedrich <friedrich.ten.hagen@googlemail.com>
2022-12-08 22:10:33 +0100

    add x to DOM and array on click

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ const gameBoard = (() => {
 
 const player = (name) => {
     this.name = name;
-    return {name}
+    this.turn = true; 
+    return {name, turn}
 }
 const player1 = player("Fred");
 const player2 = player("Johannes");
@@ -18,19 +19,28 @@ const displayControl = (() => {
             gameFields[i].textContent = gameBoard.fieldArray[i]
         }
     }
-    return {createField}
+    const addEvents = () => {
+        gameFields.forEach((field, index) => field.addEventListener("click", e => {
+            if(gameBoard.fieldArray[index]===" "){
+                field.textContent= "x"
+                gameBoard.fieldArray[index] = "x"
+            }
+        }))
+    }
+    return {createField, addEvents}
 })();
 
-
+displayControl.createField()
+displayControl.addEvents()
 // add eventlisteners to nodelist
 /* 
 player1 starts, uses "x"
 click on gameFields --> "x"
                     --> "o"
-replaces gameFields index in fieldArray (unless fieldArray[i]===" ")
+replaces gameFields index in fieldArray (unless fieldArray[i]==!" ")
     validClickCounter++ 
         validClickCounter goes from 0-9
-
+update the game: displayControl.createField
 if(validClickCounter%2==!0||validClickCounter===0) (uneven number) it is player1 turn
 
 
