aa6b1d74cbd05d9132d573d8f4c30ede4c5faaa5
Friedrich <friedrich.ten.hagen@googlemail.com>
2022-12-08 19:52:21 +0100

    update pseudo code

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
 const gameBoard = (() => {
-    const fieldArray = [];
+    const fieldArray = ["x"," ","x","x","x"," "," "," ","x"];
     return {fieldArray}
 })();
 
-function player(name){
+const player = (name) => {
     this.name = name;
     return {name}
 }
+const player1 = player("Fred");
+const player2 = player("Johannes");
+
 
 const displayControl = (() => {
+    const gameFields = document.querySelectorAll(".gameField")
     const createField = () => {
-        alert("test")
+        for(let i=0; i<gameFields.length; i++){
+            gameFields[i].textContent = gameBoard.fieldArray[i]
+        }
     }
     return {createField}
 })();
+
+
+// add eventlisteners to nodelist
+/* 
+player1 starts, uses "x"
+click on gameFields --> "x"
+                    --> "o"
+replaces gameFields index in fieldArray (unless fieldArray[i]===" ")
+    validClickCounter++ 
+        validClickCounter goes from 0-9
+
+if(validClickCounter%2==!0||validClickCounter===0) (uneven number) it is player1 turn
+
+
+*/
