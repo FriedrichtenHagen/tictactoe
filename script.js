4c7ea6afdc88a932ab2e74cb3aba3de60c6d2d9a
Friedrich <friedrich.ten.hagen@googlemail.com>
2022-12-09 20:23:08 +0100

    update the eventlisteners

--- script.js
+++ script.js
@@ -21,24 +21,28 @@ const displayControl = (() => {
     }
     let validClickCounter = 0;
     const addEvents = () => {
-        gameFields.forEach((field, index) => field.addEventListener("click", makeMark.bind(field, index))) // fix this
-    }
-    const makeMark = (field, index) => {
-        if(gameBoard.fieldArray[index]===" "){           
-            if(validClickCounter%2===0||validClickCounter===0){
-                    field.textContent= "x"
-                    gameBoard.fieldArray[index] = "x"
-                    validClickCounter++
-                    field.removeEventListener("click", makeMark())
-            }
-            else{
-                field.textContent= "o"
-                gameBoard.fieldArray[index] = "o"
-                validClickCounter++
+        let makeMark = function(field, index){
+            return function curriedFunc(e){
+                if(gameBoard.fieldArray[index]===" "){           
+                    if(validClickCounter%2===0||validClickCounter===0){
+                            field.textContent= "x"
+                            gameBoard.fieldArray[index] = "x"
+                            validClickCounter++
+                            //field.removeEventListener("click", makeMark)
+                    }
+                    else{
+                        field.textContent= "o"
+                        gameBoard.fieldArray[index] = "o"
+                        validClickCounter++
+                        //field.removeEventListener("click", makeMark)
+                    }
+                }
             }
         }
+        gameFields.forEach((field, index) => field.addEventListener("click", makeMark(field, index)))
     }
-    return {createField, addEvents}
+
+    return {createField, addEvents, validClickCounter}
 })();
 
 displayControl.createField()
@@ -50,6 +54,9 @@ displayControl.addEvents()
 if(validClickCounter%2==!0||validClickCounter===0) (uneven number) it is player1 turn
 
 add function that checks for win or draw
+if(gameBoard.fieldArray[0]==="x"&&gameBoard.fieldArray[1]==="x"&&gameBoard.fieldArray[2]==="x"){game over!}
+
+
 remove eventlistener after click
 
 
